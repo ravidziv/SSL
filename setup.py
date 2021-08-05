5f8ba1f54ad5dc05433347e7461903548d807cf0
Ziv, Ravid <ravid.ziv@intel.com>
2021-08-05 15:17:24 +0300

    update init file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='self_supervised',
-    version='0.0.1',
+    version='0.0.2',
     author='Ravid Shwartz-Ziv',
     #author_email='mike_huls@hotmail.com',
     description='Self supervised learning ',
@@ -17,5 +17,5 @@ setuptools.setup(
     },
     license='MIT',
     packages=['self_supervised'],
-    install_requires=['tensorflow'],
+    #install_requires=['tensorflow'],
 )
